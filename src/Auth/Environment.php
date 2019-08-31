acc95643a650d4d01d406f08b28acb0a0f793ebd
Lucas Assis <lucasassis@MugiwaraNoLuffy2017.local>
2019-08-30 21:46:13 -0300

    Add auth api environment class

--- src/Auth/Environment.php
+++ src/Auth/Environment.php
@@ -1,10 +1,54 @@
 <?php
 
-namespace Hotmart;
+namespace Hotmart\Auth;
 
-class ResultData
+/**
+ * Class Environment
+ *
+ * @package Hotmart\Auth
+ */
+class Environment implements \Hotmart\Environment
 {
-    
-   
-    
+    private $api;
+
+    /**
+     * Environment constructor.
+     *
+     * @param $api
+     * @param $apiQuery
+     */
+    private function __construct($api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * @return Environment
+     */
+    public static function sandbox()
+    {
+        $api = 'https://{sandbox-api-auth-url}/';
+
+        return new Environment($api);
+    }
+
+    /**
+     * @return Environment
+     */
+    public static function production()
+    {
+        $api = 'https://api-sec-vlc.hotmart.com/';
+        
+        return new Environment($api);
+    }
+
+    /**
+     * Gets the environment's Api URL
+     *
+     * @return string the Api URL
+     */
+    public function getApiUrl()
+    {
+        return $this->api;
+    }
 }
