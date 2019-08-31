f7c983b21298d853d0544da5485d8b51880c1b57
Lucas Assis <lucasassis@MugiwaraNoLuffy2017.local>
2019-08-31 14:10:14 -0300

    add correct environment to hotmart api

--- src/Api/Environment.php
+++ src/Api/Environment.php
@@ -1,10 +1,54 @@
 <?php
 
-namespace Hotmart;
+namespace Hotmart\Api;
 
-class ResultData
+/**
+ * Class Environment
+ *
+ * @package Hotmart\Api
+ */
+class Environment implements \Hotmart\Environment
 {
-    
-   
-    
+    private $api;
+
+    /**
+     * Environment constructor.
+     *
+     * @param $api
+     * @param $apiQuery
+     */
+    private function __construct($api)
+    {
+        $this->api = $api;
+    }
+
+    /**
+     * @return Environment
+     */
+    public static function sandbox()
+    {
+        $api = 'https://{sandbox-api-url}/';
+
+        return new Environment($api);
+    }
+
+    /**
+     * @return Environment
+     */
+    public static function production()
+    {
+        $api = 'https://api-hot-connect.hotmart.com/';
+        
+        return new Environment($api);
+    }
+
+    /**
+     * Gets the environment's Api URL
+     *
+     * @return string the Api URL
+     */
+    public function getApiUrl()
+    {
+        return $this->api;
+    }
 }
