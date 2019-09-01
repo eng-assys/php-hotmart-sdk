0744185656a26dda9d29eacd4f3cc2069d9d2606
Lucas Assis <lucasassis@MugiwaraNoLuffy2017.local>
2019-09-01 08:30:44 -0300

    add report examples skelethon

---
+++ examples/Report/GetSalesHistory.php
@@ -0,0 +1,22 @@
+<?php
+require '../../vendor/autoload.php';
+
+include '../.env.php';
+
+use Hotmart\Api\Environment;
+use Hotmart\Api\Hotmart;
+use Hotmart\HotConnect;
+
+$environment = Environment::production();
+
+$hotconnect = new HotConnect($envToken);
+
+try {
+    // Get The AccessToken to use HotConnect
+    $hotlinks = (new Hotmart($environment, $hotconnect))->getSalesHistory($param);
+
+    print_r($hotlinks->jsonSerialize());
+} catch (HotmartRequestException $e) {
+    print_r($e);
+}
+
