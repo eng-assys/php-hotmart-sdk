c1f952822e652d1e3297788ffebbadeeab69a7e1
Lucas Assis <eng_assys@MacBook-Pro-de-Lucas.local>
2019-11-11 14:39:48 -0300

    fix attributes of SubscriptionStatusListResponseVO object

--- src/Api/Subscription/SubscriptionStatusListResponseVO.php
+++ src/Api/Subscription/SubscriptionStatusListResponseVO.php
@@ -8,91 +8,14 @@ class SubscriptionStatusListResponseVO implements HotmartSerializable
 {
     
     /**
-     * Subscription identification code
-     * @var string
+     * @var array[SubscriptionStatusResponseVO]
      */
-    private $subscriberCode;
+    private $successSubscriptions;
     
     /**
-     * Date of subscription creation
-     * @var string|null
+     * @var array[SubscriptionStatusResponseVO]
      */
-    private $creationDate;
-    
-    /**
-     * Current recurrence number of 'maxChargeCycles'
-     * @var integer|null
-     */
-    private $currentRecurrence;
-    
-    /**
-     * Date of last billing payment
-     * @var string|null
-     */
-    private $dateLastRecurrence;
-    
-    /**
-     * Schedule date of next billing.
-     * @var string|null
-     */
-    private $dateNextCharge;
-    
-    /**
-     * Reference day for due charges. For monthly or yearly subscriptions means month day, however weekly subscriptions this field means the day of the week [1-Monday, 2-Tuesday, 3-Wednesday, 4-Thursday, 5-Friday, 6-Saturday, 7-Sunday].
-     * @var integer|null
-     */
-    private $dueDay;
-    
-    /**
-     * Number of days for trial subscription period.
-     * @var integer|null
-     */
-    private $daysToStartSubscription;
-    
-    /**
-     * Type of subscription's charge ['DAY', 'WEEK', 'MONTH', 'INVOICE'].
-     * @var string|null
-     */
-    private $intervalTypeBetweenCharges;
-
-    /**
-     * Number of days for trial subscription period.
-     * @var string|null
-     * 
-     */
-    private $maxChargeCycles;
-
-    /**
-     * Current status of subscription ['ACTIVE', 'INACTIVE']
-     * @var string|null
-     */
-    private $status;
-
-    
-    /**
-     * Date of subscription activation.
-     * @var string|null
-     */
-    private $activationDate;
-
-    /**
-     * Shopper register email address.
-     * @var string|null
-     */
-    private $emailShopper;
-
-    /**
-     * Shopper register phone number.
-     * @var string|null
-     */
-    private $phoneShopper;
-    
-    /**
-     * Reference error got when process subscription list. This tag will be only showed if got some error on process subscription.
-     * 
-     * @var string|null
-     */
-    private $error;
+    private $failSubscriptions;
 
     /**
      * @param $json
@@ -130,4 +53,52 @@ class SubscriptionStatusListResponseVO implements HotmartSerializable
     
 
     
+
+    /**
+     * Get subscription identification code
+     *
+     * @return  string
+     */ 
+    public function getSuccessSubscriptions()
+    {
+        return $this->successSubscriptions;
+    }
+
+    /**
+     * Set subscription identification code
+     *
+     * @param  string  $successSubscriptions  Subscription identification code
+     *
+     * @return  self
+     */ 
+    public function setSuccessSubscriptions(string $successSubscriptions)
+    {
+        $this->successSubscriptions = $successSubscriptions;
+
+        return $this;
+    }
+
+    /**
+     * Get date of subscription creation
+     *
+     * @return  string|null
+     */ 
+    public function getFailSubscriptions()
+    {
+        return $this->failSubscriptions;
+    }
+
+    /**
+     * Set date of subscription creation
+     *
+     * @param  string|null  $failSubscriptions  Date of subscription creation
+     *
+     * @return  self
+     */ 
+    public function setFailSubscriptions($failSubscriptions)
+    {
+        $this->failSubscriptions = $failSubscriptions;
+
+        return $this;
+    }
 }
